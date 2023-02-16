ad3f39e7158d7b2e2a7ca6d0adeea22bce22ca75
Yoana Chung <yoana@yoanachung.com>
2023-02-16 09:55:14 -0800

    firebase todo

--- assets/i18n.8aa423eb.js
+++ assets/i18n.8aa423eb.js
@@ -1,4 +1,4 @@
-import{h as Xe,e as wt,r as Q,c as q,w as ce,F as Ut,i as Wt,g as Ye,a as Vt,o as xt,b as jt,d as Gt,T as $t,f as Ht}from"./index.bc6a6312.js";/*!
+import{h as Xe,e as wt,r as Q,c as q,w as ce,F as Ut,i as Wt,g as Ye,a as Vt,o as xt,b as jt,d as Gt,T as $t,f as Ht}from"./index.d3a5d516.js";/*!
   * shared v9.2.2
   * (c) 2022 kazuya kawaguchi
   * Released under the MIT License.
